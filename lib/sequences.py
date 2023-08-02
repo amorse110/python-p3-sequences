7f090f814111c640a32310542992aadfa3a83a73
Austin Morse <amorse110@gmail.com>
2023-08-02 14:05:14 -0700

    lab complete

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        print([])
+    elif length == 1:
+        print([0])
+    else:
+        init = [0, 1]
+        for num in range(1, length-1):
+            new_number = init[num-1] + init[num]
+            init.append(new_number)
+        print(init)
